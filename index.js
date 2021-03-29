fef3b84db135002fb6cfc8af2c44775f707342e0
farhan-nahid <nahidahmed9933@gmail.com>
2021-03-29 14:58:37 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express')
-const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors')
+require ( 'dotenv' ).config()
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xoju.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors);
 const port = 5000;
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const products = client.db("emaJohnStore").collection("products");
+  
+  app.post('products', (res,req)=>{
+    const product = req.body;
+    products.insertOne(product)
+    .then(result =>{
+      console.log(result);
+    })
+
+  })
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello Ema Watson!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port)
